a23f374eda2416a68cea49ece04eefbf25d75105
JJW <jonathanwubrave@gmail.com>
2021-10-30 23:16:07 -0400

    welcome page

---
+++ src/components/TrendingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+function TrendingPage() {
+    return (
+        <div>
+            <Grid>  
+
+            </Grid>
+            
+        </div>
+    )
+}
+// https://www.youtube.com/watch?v=UNCq01LNNrg&t=236s
+
+export default TrendingPage
